f0c27e9ad99e32f45b74b2fcc9bb617896ba6f5a
LewieJ08 <lewiej08@gmail.com>
2025-05-14 18:17:08 +0100

    loading env variables

--- motorsport_app/database.py
+++ motorsport_app/database.py
@@ -1,3 +1,11 @@
 from dotenv import load_dotenv
 import os
 import psycopg2
+
+load_dotenv()
+
+host = os.getenv("HOST")
+dbname = os.getenv("DBNAME")
+user = os.getenv("USER")
+password = os.getenv("PASSWORD")
+port = os.getenv("PORT")
