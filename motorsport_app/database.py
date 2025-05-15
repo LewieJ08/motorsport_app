a76502a27c88e2822dbbfc05f02bfa50376bce00
LewieJ08 <lewiej08@gmail.com>
2025-05-15 17:01:01 +0100

    initial db functions

--- motorsport_app/database.py
+++ motorsport_app/database.py
@@ -9,3 +9,15 @@ dbname = os.getenv("DBNAME")
 user = os.getenv("USER")
 password = os.getenv("PASSWORD")
 port = os.getenv("PORT")
+
+def get_connection():
+    return psycopg2.connect(host=host, dbname=dbname, user=user, password=password, port=port)
+
+def init_database():
+    with open("schema.sql", "r") as file:
+        schema = file.read()
+
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(schema)
+            conn.commit()
